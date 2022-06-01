de1c6f064eb175fee7c6e094906f5632f6568213
Kelly Chang <u2kelly619@gmail.com>
2022-06-01 10:12:15 +0800

    modify test 2

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<head><meta charset="utf-8" /></head>')
     res.write('<body>')
     res.write('<h1>這是首頁</h1>')
     res.write('</body>')
-});
+})
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
